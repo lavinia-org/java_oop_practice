7336098720c0b23fd314416c5df51e191bbb7301
Larisa Alexa <larisa.alexa@softvision.com>
2020-09-08 16:42:59 +0300

    Define a class - Person

---
+++ src/main/java/person/ManagePerson.java
@@ -0,0 +1,9 @@
+package person;
+
+import person.Person;
+
+public class ManagePerson {
+    public static void main(String[] args){
+        Person larisaPerson = new Person();
+    }
+}
---
+++ src/main/java/person/Person.java
@@ -0,0 +1,11 @@
+package person;
+
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String gender;
+    private String emailAddress;
+
+}
