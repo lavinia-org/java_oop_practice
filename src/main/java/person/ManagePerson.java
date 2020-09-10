ade2d8f047d09827cc0482fdd5d1b07ffef47e78
Larisa Alexa <larisa.alexa@softvision.com>
2020-09-10 16:16:45 +0300

    exercise 3

--- src/main/java/person/ManagePerson.java
+++ src/main/java/person/ManagePerson.java
@@ -5,5 +5,6 @@ import person.Person;
 public class ManagePerson {
     public static void main(String[] args){
         Person larisaPerson = new Person("Larisa", "Alexa", 31, "Female", "larisa.alexa@softvision.com");
+        System.out.println(larisaPerson.getFirstName() + " " + larisaPerson.getLastName());
     }
 }
