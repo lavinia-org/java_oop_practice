cb86f5c0a87d54217eca6fe3f985d7360e04aaf2
Larisa Alexa <larisa.alexa@softvision.com>
2020-09-11 14:48:41 +0300

    exercise 6

--- src/main/java/person/Person.java
+++ src/main/java/person/Person.java
@@ -65,5 +65,9 @@ public class Person {
             System.out.println("Error! Please enter a valid email address!");
         }
     }
-    
+
+    public void printPersonDetails(){
+        System.out.println(firstName + ", " + lastName + ", " + age + ", " + gender + ", " + emailAddress);
+    }
+
 }
