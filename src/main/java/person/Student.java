2a3816309847bb6c5e1b8f86574b0f5fbcc743a3
Larisa Alexa <larisa.alexa@softvision.com>
2020-09-14 15:54:28 +0300

    exercise 1

---
+++ src/main/java/person/Student.java
@@ -0,0 +1,12 @@
+package person;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Student extends Person {
+   private Boolean graduated;
+   private static String code;
+   private int interviewScore;
+   private Map<String, Integer> disciplineMarks = new HashMap<>();
+
+}
